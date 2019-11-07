ab1a209aa7c7a01b6d52d81edc8c85ea3d6c94d1
Nikz Jon <nikhiljohn1010@gmail.com>
2019-11-07 18:15:40 +0530

    Added webpage

---
+++ public/js/validation.js
@@ -0,0 +1,58 @@
+/*
+
+Template:  Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template
+Author: potenzaglobalsolutions.com
+Design and Developed by: potenzaglobalsolutions.com
+
+NOTE: 
+
+*/
+
+(function($) {
+  "use strict";
+  $.validator.setDefaults({
+    submitHandler: function() {
+      validator.resetForm();
+      alert("submitted!");
+    }
+  });
+  $(document).ready(function() {
+    $("#apiTestForm").validate({
+      rules: {
+        apitoken: {
+          required: true,
+          minlength: 40
+        },
+        rname: "required",
+      },
+      messages: {
+        apitoken: {
+          required: "Please enter your API Token",
+          minlength: "Invalid API Token"
+        },
+        rname: {
+          required: "Please enter a valid FQDN",
+          url: true
+        },
+      },
+      errorElement: "em",
+      errorPlacement: function(error, element) {
+        // Add the `help-block` class to the error element
+        error.addClass("help-block");
+
+        if (element.prop("type") === "checkbox") {
+          error.insertAfter(element.parent("label"));
+        } else {
+          error.insertAfter(element);
+        }
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
+      }
+    });
+  });
+
+})(jQuery);
